f259daccac289f2fc146da102672e64196f70459
jonas <jonas.grondahl@wexer.com>
2018-10-30 15:19:28 +0100

    boom

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,72 @@
+
+// Production steps of ECMA-262, Edition 5, 15.4.4.18
+// Reference: http://es5.github.io/#x15.4.4.18
+if (!Array.prototype.forEach) {
+    console.log("adding forEach");
+    Array.prototype.forEach = function (callback/*, thisArg*/) {
+
+        var T, k;
+
+        if (this == null) {
+            throw new TypeError('this is null or not defined');
+        }
+
+        // 1. Let O be the result of calling toObject() passing the
+        // |this| value as the argument.
+        var O = Object(this);
+
+        // 2. Let lenValue be the result of calling the Get() internal
+        // method of O with the argument "length".
+        // 3. Let len be toUint32(lenValue).
+        var len = O.length >>> 0;
+
+        // 4. If isCallable(callback) is false, throw a TypeError exception. 
+        // See: http://es5.github.com/#x9.11
+        if (typeof callback !== 'function') {
+            throw new TypeError(callback + ' is not a function');
+        }
+
+        // 5. If thisArg was supplied, let T be thisArg; else let
+        // T be undefined.
+        if (arguments.length > 1) {
+            T = arguments[1];
+        }
+
+        // 6. Let k be 0.
+        k = 0;
+
+        // 7. Repeat while k < len.
+        while (k < len) {
+
+            var kValue;
+
+            // a. Let Pk be ToString(k).
+            //    This is implicit for LHS operands of the in operator.
+            // b. Let kPresent be the result of calling the HasProperty
+            //    internal method of O with argument Pk.
+            //    This step can be combined with c.
+            // c. If kPresent is true, then
+            if (k in O) {
+
+                // i. Let kValue be the result of calling the Get internal
+                // method of O with argument Pk.
+                kValue = O[k];
+
+                // ii. Call the Call internal method of callback with T as
+                // the this value and argument list containing kValue, k, and O.
+                callback.call(T, kValue, k, O);
+            }
+            // d. Increase k by 1.
+            k++;
+        }
+        // 8. return undefined.
+    };
+}
+
 $("#calendar").fullCalendar({
     defaultView: "agendaWeek",
     allDaySlot: false,
     height: 'auto',
-    editable: true,
     eventDurationEditable: false,
     slotDuration: "00:30:00",
     slotLabelInterval: "01:00:00",
@@ -18,11 +82,14 @@ $("#calendar").fullCalendar({
     viewRender: function () {
         const nodes = document.querySelectorAll("#calendar tbody tr:nth-child(2n) td:first-child");
         const nodes1 = document.querySelectorAll("#calendar tbody tr:nth-child(2n-1) td:first-child span");
-        nodes.forEach(n => {
-            n.innerHTML = '<span class="small">30</span>';
-        });
-        nodes1.forEach(n => {
-            n.innerHTML += "<span class='small'>00</span>";
-        });
+        for (let i = 0; i < nodes.length; i++) {
+
+            nodes[i].innerHTML = '<span class="small">30</span>';
+        };
+        for (let i = 0; i < nodes1.length; i++) {
+
+
+            nodes1[i].innerHTML += "<span class='small'>00</span>";
+        };
     }
 });
